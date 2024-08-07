c3ec02802e071f96687507a5daf0b85cd8837833
Wiwat <seawaysia@gmail.com>
2024-08-07 17:57:34 +0700

    config docker-compose hot reload all

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ app.get('/', (req,res) =>{
     res.status(200).send("Hello worlaxascscxd!!")
 })
 app.get('/test', (req,res) =>{
-    res.status(200).send("Hello testawdwdaa!!")
+    res.status(200).send("Hello !!")
 })
 app.listen(process.env.PORT_BE)
 console.log('Server on port ',process.env.PORT_BE)
