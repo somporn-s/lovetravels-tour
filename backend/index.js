dcccbbdf39b85b128098b7f4995c61262621ed25
Wiwat <seawaysia@gmail.com>
2024-08-07 17:47:27 +0700

    update dockerfile for hot reload backend

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) =>{
-    res.status(200).send("Hello world!!")
+    res.status(200).send("Hello worlaxascscxd!!")
+})
+app.get('/test', (req,res) =>{
+    res.status(200).send("Hello testawdwdaa!!")
 })
 app.listen(process.env.PORT_BE)
 console.log('Server on port ',process.env.PORT_BE)
